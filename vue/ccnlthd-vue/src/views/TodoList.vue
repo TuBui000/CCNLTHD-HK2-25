6147a129f1dffcb905e3dac7ba229cc9800bcc12
TuBui000 <129721838+TuBui000@users.noreply.github.com>
2025-03-06 14:42:52 +0700

    update-tb-06/03/25

--- vue/ccnlthd-vue/src/views/TodoList.vue
+++ vue/ccnlthd-vue/src/views/TodoList.vue
@@ -1,14 +1,36 @@
 <script setup>
+import HomeView from './HomeView.vue';
+
+
+const home = {HomeView, path:"/"}
 
 </script>
 
 <template>
-    <div class="bg-dark d-flex align-items-center justify-content-center vh-100">
-        <div class="container text-center p-4">
-            <div class="justify-content-center g-0">
-                <h2 class="text-bg-dark">Todo List</h2>
+    <div class="bg-todo d-flex align-items-center justify-content-center vh-100">
+        <div class="container text-center w-50">
+            <div class="d-flex justify-content-start">
+               <RouterLink class="btn btn-primary" :to="home.path"></RouterLink>
             </div>
-            
+            <div class="justify-content-center">
+                <h2 class="text-dark">TODO LIST</h2>
+            </div>
+            <div class="input-todo mt-3">
+                <div class="input-gr mb-3 w-50">
+                    <input type="text" placeholder="Add new todo" aria-label="Add new todo"
+                        aria-describedby="button-addtodo">
+                    <button type="button" id="button-addtodo">
+                        <i class="fa-solid fa-plus" ></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card-todo mt-4 p-4">
+                <div class="d-flex justify-content-start">
+                    <h4 class="fst-italic">Todo's</h4>
+                </div>
+                <div class="">
+                </div> 
+            </div>
         </div>
     </div>
 </template>
